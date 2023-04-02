2ba4e52c6753bf454bd85c3886136be80d4a6489
Apoorv Verma <vermasunny224@gmail.com>
2023-04-02 14:35:30 +0530

    techs

--- models/Interest.js
+++ models/Interest.js
@@ -9,7 +9,7 @@ const Interest = mongoose.model('Interest', new mongoose.Schema(
     },
 ),"interests");
 
-const intrests = [ "Angular", "Node JS", "Express JS","MongoDB" ];
+const intrests = ["javascript", "python", "ruby", "java", "html", "css", "php", "shell", "simple", "library", "objective-c", "android", "go", "files", "framework", "c++", "swift", "c", "build", "react", "server", "client", "c#", "google", "example", "docker", "image", "angular", "rails", "website", "system", "awesome", "collection", "browser", "theme", "scala", "open-source", "tools", "typescript", "design", "extension", "component", "clojure", "development", "coffeescript", "jquery", "language", "graph", "search", "django", "programming", "bootstrap", "generator", "configuration", "network", "elixir", "viml", "manager", "testing", "cloud" ];
 
 intrests.forEach(async item => {
     await Interest.findOneAndUpdate({ name: item  }, { slug: slug(item) }, {  new: true,upsert: true })
