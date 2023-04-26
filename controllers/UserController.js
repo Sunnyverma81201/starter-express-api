02b7a762a3ceca5d6bc5fe55c28f6ef9517b8826
Apoorv Verma <vermasunny224@gmail.com>
2023-04-26 09:16:42 +0530

    jwt error debug

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -205,6 +205,7 @@ const noInterest = async (user,tech) => {
 export const getUser = async (req,res) => {
 
     const user = await User.findById(req.body.id,"-password").populate('img').exec()
+    console.log(user)
     res.send(user)
 }
 
