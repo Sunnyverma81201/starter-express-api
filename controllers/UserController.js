a23a15b68e925c8e3f7b22776298e4055132425d
Apoorv Verma <vermasunny224@gmail.com>
2023-05-04 16:59:49 +0530

    invite user accept fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -115,7 +115,7 @@ export const projectInvite = async (req,res) => {
 }
 
 export const acceptProjectInvite = async (req,res) => {
-    let data = await  InviteUser.findById(req.body.inviteId);
+    let data = await InviteUser.findById({user: req.body.userId});
 
     if(data == null)
         res.status(450);
